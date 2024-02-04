1af9889d9279e77a3de88b040d06259d45c93c97
Huzaifah Farooq <hf521@ic.ac.uk>
2024-02-04 02:29:54 +0000

    Refactor the homescreen and add a progress bar

--- sidequests/lib/view/home_view.dart
+++ sidequests/lib/view/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sidequests/backend/tables/tasks.dart';
 import 'package:sidequests/controller/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -10,53 +11,57 @@ class HomeView extends GetView<HomeController> {
     final model = controller.model;
 
     return Scaffold(
+      appBar: AppBar(
+        title: Obx(
+          () => LinearProgressIndicator(
+            value: model.completedTasks.length /
+                (model.completedTasks.length + model.pendingTasks.length),
+          ),
+        ),
+        leading: Obx(
+          () => Center(child: Text(model.completedTasks.length.toString())),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.addTask,
+        child: const Icon(Icons.add),
+      ),
       body: SafeArea(
         child: Center(
           child: CustomScrollView(
             slivers: [
-              SliverToBoxAdapter(
-                child: IconButton(
-                  onPressed: controller.addTask,
-                  icon: const Icon(Icons.add),
-                ),
-              ),
-              Obx(
-                () => SliverList.builder(
-                  itemCount: model.pendingTasks.length,
-                  itemBuilder: (context, index) => Dismissible(
-                    key: Key(model.pendingTasks[index].description),
-                    onDismissed: (direction) =>
-                        controller.deleteTask(model.pendingTasks[index]),
-                    child: CheckboxListTile(
-                      title: Text(
-                        model.pendingTasks[index].description,
-                      ),
-                      value: model.pendingTasks[index].completed,
-                      onChanged: (val) =>
-                          controller.checkTask(model.pendingTasks[index], val!),
-                    ),
-                  ),
-                ),
-              ),
-              Obx(
-                () => SliverList.builder(
-                  itemCount: model.completedTasks.length,
-                  itemBuilder: (context, index) => Dismissible(
-                    key: Key(model.completedTasks[index].description),
-                    onDismissed: (direction) =>
-                        controller.deleteTask(model.completedTasks[index]),
-                    child: CheckboxListTile(
-                      title: Text(
-                        model.completedTasks[index].description,
-                      ),
-                      value: model.completedTasks[index].completed,
-                      onChanged: (val) => controller.checkTask(
-                          model.completedTasks[index], val!),
-                    ),
-                  ),
-                ),
-              )
+              TaskTile(taskList: model.pendingTasks),
+              TaskTile(taskList: model.completedTasks),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TaskTile extends GetView<HomeController> {
+  const TaskTile({
+    required this.taskList,
+    super.key,
+  });
+
+  final RxList<TaskRecord> taskList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => SliverList.builder(
+        itemCount: taskList.length,
+        itemBuilder: (context, index) => Dismissible(
+          key: Key(taskList[index].description),
+          onDismissed: (direction) => controller.deleteTask(taskList[index]),
+          child: CheckboxListTile(
+            title: Text(
+              taskList[index].description,
+            ),
+            value: taskList[index].completed,
+            onChanged: (val) => controller.checkTask(taskList[index], val!),
           ),
         ),
       ),
